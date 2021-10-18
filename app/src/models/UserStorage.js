18586cbb1f8ca05a76d36c0ae411d1272daf173f
khsunkh <khsunkh@gmail.com>
2021-10-18 22:42:20 +0000

    User 모델 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -18,9 +18,20 @@ class UserStorage {
                 return newUsers; // reduce 내에서 반환해주어야 newUsers가 갱신됨
             }, {}); // 빈 {}는 reduce 안의 newUsers의 초기값
             return newUsers;
-        }
+        };
         return users;
     };
-}
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const userKeys = Object.keys(users); // => [id, psword, name]
+        const userInfo = userKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+        return userInfo;
+    };
+};
 
 module.exports = UserStorage;
--- app/src/routes/home/home.ctl.js
+++ app/src/routes/home/home.ctl.js
@@ -1,6 +1,7 @@
 "use strict"
 
-const UserStorage = require('../../models/UserStorage');
+const User = require('../../models/User');
+// const UserStorage = require('../../models/UserStorage');
 
 const view = {
     home: (req, res) => {
@@ -13,22 +14,8 @@ const view = {
 
 const process = {
     login: (req, res) => {
-        const id = req.body.id,
-            psword = req.body.psword;
-
-        const users = UserStorage.getUsers("id", "psword");
-        
-        const response = {};
-        if (users.id.includes(id)) {
-            const idx = users.id.indexOf(id);
-            if (users.psword[idx] === psword) {
-                response.success = true;
-                return res.json(response);
-            }
-        }
-
-        response.success = false;
-        response.msg = "로그인에 실패하셨습니다.";
+        const user = new User(req.body);
+        const response = user.login();
         return res.json(response);
     },
 };
@@ -44,3 +31,4 @@ module.exports = {
 //     hello: hello,
 //     login: login,
 // };
+
